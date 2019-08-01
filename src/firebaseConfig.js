a998ebb39c8aace4f3b02126ce04dd42787f28ab
lindaMontoya <48225201+lindaMontoya@users.noreply.github.com>
2019-08-01 10:31:35 -0500

    Initial commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDuJiOUt3uQSkJzpWSjA5dKZdgNJ5wtoc4",
+    authDomain: "login-c4be4.firebaseapp.com",
+    databaseURL: "https://login-c4be4.firebaseio.com",
+    projectId: "login-c4be4",
+    storageBucket: "",
+    messagingSenderId: "469799054234",
+    appId: "1:469799054234:web:fae5c0b9862bcc4b"
+  };
